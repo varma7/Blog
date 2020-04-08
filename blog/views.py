6d660ef781b19d004e26b015365033f3d3b7b2d0
varma7 <pradeepvarma107@gmail.com>
2020-04-08 11:41:30 +0530

    Add Db to View + Fix Bug
    Fix Bug : HttpResponseRequest as Root
    
    Signed-off-by: varma7 <pradeepvarma107@gmail.com>

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from blog.forms import CreatePostForm
+from blog.models import BlogModel
+from django.http import HttpResponseRedirect
 
 def index(request):
-    return render(request,'blog/index.html')
+    val = BlogModel.objects.order_by('title')
+    context = {'key':val}
+    return render(request,'blog/index.html',context)
 
 def createPostView(request):
     form = CreatePostForm()
@@ -10,7 +14,8 @@ def createPostView(request):
         form = CreatePostForm(request.POST)
         if form.is_valid():
             form.save(commit=True)
-            return index(request)
+            return HttpResponseRedirect('/')
         else:
             print('Error')
+    index(request)
     return render(request,'blog/create.html',{'key':form})
